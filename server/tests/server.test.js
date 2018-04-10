8d022b27e4fbd96720ba2b07128800cc6947e536
raghavmalawat <raghavmalawat@gmail.com>
2018-04-10 21:43:08 +0530

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -139,3 +139,42 @@ describe('DELETE /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('PATCH /todos/:id' ,()=>{
+  it('should update the todo',(done)=>{
+    var hexId =todos[0]._id.toHexString();
+    var text='This should be new text';
+    request(app)
+    .patch(`/todos/${hexId}`)
+    .send({
+      completed:true,
+      text
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo.text).toBe(text)
+      expect(res.body.todo.completed).toBe(true)
+      expect(typeof res.body.todo.completedAt).toBe('number');
+    })
+    .end(done);
+
+  });
+  it('should clear completedAt when Todo is not completed',(done)=>{
+    var hexId =todos[0]._id.toHexString();
+    var text='This should be new text!!';
+
+    request(app)
+    .patch(`/todos/${hexId}`)
+    .send({
+      completed:false,
+      text
+    })
+    .expect(200)
+    .expect((res)=>{      
+      expect(res.body.todo.text).toBe(text)
+      expect(res.body.todo.completed).toBe(false)
+      expect(res.body.todo.completedAt).toBe(null);
+    })
+    .end(done);
+  });
+});
